2c74f2743b85d0715b1416f8f90a742d13dc43cc
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-09-02 13:33:00 +0530

    update footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           <a
-            href="https://twitter.com/womenbuildweb3"
+            href="https://twitter.com/Aayush_gupta_ji"
             className="text-gray-400 hover:text-gray-500"
             target="_blank"
             rel="noopener noreferrer"
@@ -22,7 +22,7 @@ export default function Footer() {
             </svg>
           </a>
           <a
-            href="https://github.com/womenbuildweb3/lens-api-tutorial"
+            href="https://github.com/AAYUSH-GUPTA-coder/Lens-Social-Media-dapp"
             className="text-gray-400 hover:text-gray-500"
             target="_blank"
             rel="noopener noreferrer"
